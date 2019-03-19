6cf65f40bf2a7d95c189ec44ea4f8879f7b2da6b
Braden Niksich <abniksi@ilstu.edu>
2019-03-19 12:41:31 -0500

    Quick fixes

--- JN Photography V2/javascript/bnphotgraphy.js
+++ JN Photography V2/javascript/bnphotgraphy.js
@@ -1,10 +1,23 @@
+/* Setting a variable that helps switch classes. */
+let x = 0;
+
 /* Creating a function called navbarSwitch. */
 function navbarSwitch() {
-  /* Selecting navbarItems class with DOM manipulation. */
-  var navbarToggle = document.querySelectorAll('.navbarItems');
-
-  /* Toggle between navbarItems class and navbarToggleShow. */
+  /* Selecting navbarItems class and font ID with DOM manipulation. */
+  let navbarToggle = document.querySelectorAll('.navbarItems');
+  let font = document.querySelector('#font');
+  /* Toggle navbarToggleShow being in the class and not in the class. */
   navbarToggle.forEach(nav => nav.classList.toggle('navbarToggleShow'));
+  /* Changing icons. */
+  if(x === 0){
+    font.classList.remove('fa-bars');
+    font.classList.add('fa-window-close');
+    x = 1;
+  } else {
+    font.classList.add('fa-bars');
+    font.classList.remove('fa-window-close');
+    x = 0;
+  }
 }
 
 /* When user clicks on navbarLink-toggle button, calls the navbarSwitch function
