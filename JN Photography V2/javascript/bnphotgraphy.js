631050d39527c7fbc61ea2b017c1c70aab7f99c1
Braden Niksich <abniksi@ilstu.edu>
2019-03-18 21:41:25 -0500

    Complete revamp

---
+++ JN Photography V2/javascript/bnphotgraphy.js
@@ -0,0 +1,12 @@
+/* Creating a function called navbarSwitch. */
+function navbarSwitch() {
+  /* Selecting navbarItems class with DOM manipulation. */
+  var navbarToggle = document.querySelectorAll('.navbarItems')
+
+  /* Toggle between navbarItems class and navbarToggleShow. */
+  navbarToggle.forEach(nav => nav.classList.toggle('navbarToggleShow'));
+}
+
+/* When user clicks on navbarLink-toggle button, calls the navbarSwitch function
+and shows the other pages the user can visit. */
+document.querySelector('.navbarLink-toggle').addEventListener('click', navbarSwitch);
--- JN Photography V2/jnphotgraphy.js
+++
@@ -1,6 +0,0 @@
-function classToggle() {
-  const navs = document.querySelectorAll('.navbarItems')
-
-  navs.forEach(nav => nav.classList.toggle('navbarToggleShow'));
-}
-document.querySelector('.navbarLink-toggle').addEventListener('click', classToggle);
